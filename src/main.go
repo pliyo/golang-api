01010d3f03098a23fc10a0584981f0c3a2d4c3c4
Juanjo Cerezuela <juanjo.cerezuela@guestline.com>
2018-08-24 17:55:59 +0200

    Adding httprouter, negroni, main...

--- src/main.go
+++ src/main.go
@@ -2,6 +2,22 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/codegangsta/negroni"
+	"github.com/julienschmidt/httprouter"
+)
+
+func MyHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hello %v\n", ps.ByName("id"))
+}
+
+func main() {
+	mux := httprouter.New()
+	mux.GET("/some/page/:id", MyHandler)
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(":3000")
 }
